73e13c687f3c049f16e8d137504f2991da5c2a8b
nagorecb <nagorecb@opendeusto.es>
2019-11-01 18:59:17 +0100

    ServletContainer errors fixed

--- src/main/java/Biwanger/AppService/clsAppServiceAdmin.java
+++ src/main/java/Biwanger/AppService/clsAppServiceAdmin.java
@@ -3,7 +3,6 @@ package Biwanger.AppService;
 import Biwanger.ObjetosDominio.clsUsuario;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
--- src/main/java/Biwanger/Facade/ServletContainer.java
+++ src/main/java/Biwanger/Facade/ServletContainer.java
@@ -1,6 +1,6 @@
 package Biwanger.Facade;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -27,39 +27,27 @@ public class ServletContainer
     }
 
     @POST
-    @Path("/LoginRequest")
+    @Path("/login")
     public Response LoginRequest(String email, String password)
     {
-        clsUsuario u = new clsUsuario();//Sacar de la BD el usuario con ese mail y password
-
-       // String retorno = userService.InicioSesion(u);
-        Response retorno = new Response();
-        return retorno;
+        clsUsuario usuario = new clsUsuario();//select: Sacar de la BD el usuario con ese mail y password
+        return Response.ok(usuario).build();
     }
 
     @POST
-    @Path("/RegistroRequest")
+    @Path("/registro")
     public Response RegistroRequest(String email, String password)
     {
-        //boolean retorno = userService.RegistrarUser(email, password);
-        Response retorno = new Response();
-        return retorno;
+        boolean retorno = userService.RegistrarUser(email, password);
+        return Response.ok(retorno).build();
     }
 
     @GET
-    @Path("/PremiarTresMejores")
+    @Path("/premiarTresMejores")
     public Response PremiarTresMejores()
     {
-      //  ArrayList <clsUsuario> listaUsuarios = userService.PremiarTresMejores();
-        Response retorno = new Response();
-        return listaUsuarios;
-    }
-    
-    @GET
-    @Path("/getRequest")
-    public Response getRequest()
-    {
-
+    	List <clsUsuario> listaUsuarios = adminService.PremiarTresMejores();
+        return Response.ok(listaUsuarios).build();
     }
 
 }
