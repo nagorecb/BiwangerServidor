074d980ae48eed05c3867f3e34a0c155b750e045
unknown <maiderc@opendeusto.es>
2019-12-22 17:25:30 +0100

    comments for ServletContainer added

--- src/main/java/Biwanger/Facade/ServletContainer.java
+++ src/main/java/Biwanger/Facade/ServletContainer.java
@@ -15,6 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * @brief Clase que conecta al cliente con el servidor
+ * @details Clase que ofrece los microservicios del servidor al cliente, funcionando como fachada que recoge las petidiones del cliente y redirige a los distintos appService
+ */
 @Path("/resource")
 @Produces(MediaType.APPLICATION_JSON)
 public class ServletContainer
@@ -22,14 +26,17 @@ public class ServletContainer
     clsDAO dao = clsDAO.getInstance();
 	clsAppServiceAdmin adminService = clsAppServiceAdmin.getInstance();
 	clsAppServiceUser userService = clsAppServiceUser.getInstance();
-	clsHiloPujas hilo = clsHiloPujas.getInstance();
 
-	 
-    public ServletContainer()
-    {
+    /**
+     * Constructor sin parámetros
+     */
+    public ServletContainer() { }
 
-    }
-
+    /**
+     * Método que recoge las peticiones de inicio de sesión
+     * @param usuario usuario que quiere iniciar sesión
+     * @return devuelve un Response con el usuario devuelto por userService
+     */
     @POST
     @Path("/login")
     public Response LoginRequest(clsUsuario usuario)
@@ -38,6 +45,11 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método que recoge las peticiones de registro
+     * @param usuario usuario que se quiere registrar
+     * @return devuelve un Response con el String que indica si se ha podido o no realizar la acción
+     */
     @POST
     @Path("/registro")
     public Response RegistroRequest(clsUsuario usuario)
@@ -48,6 +60,10 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método que devuelve a la petición de premiar a los tres mejores usuarios
+     * @return devuelve un Response con la lista de usuarios una vez premiados
+     */
     @GET
     @Path("/premiarTresMejores")
     public Response PremiarTresMejores()
@@ -59,21 +75,38 @@ public class ServletContainer
 
         return Response.ok(retorno).build();
     }
-    
+
+    /**
+     * Método que responde a las peticiones de modificar alineación
+     * @param plantilla jugadores cuyos datos se van a actualizar
+     * @return Response que indica que se ha podido realizar la operación
+     */
     @POST
     @Path("/modificarAlineacion")
-    public void modificarAlineacion(clsJugadorLista plantilla)
+    public Response modificarAlineacion(clsJugadorLista plantilla)
     {
         userService.modificarAlineacion(plantilla.getlJugadores());
+
+        return Response.ok().build();
     }
 
+    /**
+     * Método que responde a las peticiones de odificar la formación de un usuario
+     * @param usuario usuario cuya formación se va a actualizar
+     * @return Response que indica que se ha podido realizar la operación
+     */
     @POST
     @Path("/modificarFormacion")
-    public void modificarFormacion(clsUsuario usuario)
+    public Response modificarFormacion(clsUsuario usuario)
     {
         userService.modificarFormacion(usuario);
+        return Response.ok().build();
     }
 
+    /**
+     * Método que responde a la petición de obtener toos los jugadores en venta
+     * @return devuelve una lista con los jugadores en venta dentro del Response
+     */
     @GET
     @Path("/MostrarMercado")
     public Response MostrarMercado()
@@ -85,6 +118,11 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método que responde a la petición de almacenar una puja realizada
+     * @param puja puja a guardar
+     * @return devuelve un Response indicando si se ha podido realizar la operación
+     */
     @POST
     @Path("/pujar")
     public Response Pujar(clsPuja puja)
@@ -93,6 +131,11 @@ public class ServletContainer
         return Response.ok().build();
     }
 
+    /**
+     * Método que responde a las peticiones de vender jugadores
+     * @param jugadorVenta jugador que se quiere vender
+     * @return dvuelve un Response inidicando si se ha podido realizar la operación
+     */
     @POST
     @Path("/VenderJugador")
     public Response venderJugador(clsJugador jugadorVenta)
@@ -100,7 +143,13 @@ public class ServletContainer
         userService.venderJugador(jugadorVenta);
         return Response.ok().build();
     }
-    
+
+    /**
+     * Método que responde a la petición de añadir puntos a los jugadores
+     * @param idJugador identificativo del jugador al que se le quieren añadir los puntos
+     * @param puntosAnadir puntos que se le quieren añadir al jugador
+     * @return devuelve un Response indicando que la operación se ha podido realizar
+     */
     @POST
     @Path("/anadirPuntos")
     public Response anadirPuntosRequest(@FormParam("idJugador") int idJugador, @FormParam("puntos") int puntosAnadir)
@@ -108,7 +157,12 @@ public class ServletContainer
         adminService.anadirPuntos(idJugador, puntosAnadir);
         return Response.ok().build();
     }
-    
+
+    /**
+     * Método que responde a las peticiones del administrador de crear jugadores
+     * @param camposJugador String con todos los campos introducidos para la creación del jugador
+     * @return devuelve un Response indicando que la operación se ha realizado con éxito
+     */
     @POST
     @Path("/crearJugador")
     public Response crearJugadorRequest(String camposJugador)
@@ -129,6 +183,10 @@ public class ServletContainer
         return Response.ok().build();
     }
 
+    /**
+     * Método que responde a las peticiones para obtener los usuarios
+     * @return Devuelve un response con la lista de todos los usuarios
+     */
     @GET
     @Path("/obtenerTodosUsuarios")
     public Response obtenerTodosUsuarios()
@@ -140,6 +198,10 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método que responde a las peticiones para obtener los jugadores
+     * @return Devuelve un response con la lista de todos los jugadores
+     */
     @GET
     @Path("/obtenerTodosJugadores")
     public Response obtenerTodosJugadores()
@@ -151,6 +213,11 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método para responder a las peticiones de obtener la plantilla de un usuario
+     * @param email identificador del usuario cuya plantilla se quiere obtener
+     * @return devuelve un Response con la plantilla del usuario
+     */
     @POST
     @Path("/obtenerPlantilla")
     public Response obtenerPlantilla(String email)
@@ -159,6 +226,11 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método que responde a las peticiones de obtener la clasificación de jugadores de un usuario
+     * @param email identificativo del usuario cuya plantilla se quiere ordenar
+     * @return devuelve un Response con la lista de jugadores del usuario ordenada
+     */
     @POST
     @Path("/clasificacionEquipo")
     public Response clasificacionEquipo(String email)
@@ -169,6 +241,10 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método que responde a la petición de obtener la lista de todos los usuarios ordenados por puntuación
+     * @return devuelve un Response con la lista de usuarios ordenada
+     */
     @GET
     @Path("/clasificacionUsuarios")
     public Response clasificacionUsuarios()
@@ -178,6 +254,9 @@ public class ServletContainer
         return Response.ok(retorno).build();
     }
 
+    /**
+     * Método para introducir ciertos datos en la base de datos para tener datos de prueba al iniciar el programa
+     */
     @GET
     @Path("/hardcode")
     public void hardcode()
